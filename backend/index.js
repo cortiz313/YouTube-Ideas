c0a1f82c6b9933ff3f7216aeb3807258179f1e31
Christian Ortiz <c.ortiz313@gmail.com>
2023-09-08 14:12:33 -0400

    Setting up idea model, postman
    Working on setting up the model, and the post request to create a new idea. Began connecting and calling post request with postman

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,49 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Idea } from "./models/ideaModel.js";
 
 const app = express();
+
+// Middlewarefor parsing request body
+app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to MERN stack tutorial");
+});
+
+// Route to save a new idea
+// POST to create a new idea
+// TODO: Connect this to the YouTube process or ChatGPT process
+// Mongoose is async, so can use it
+app.post("/idea", async (request, response) => {
+  try {
+    if (
+      !request.body.idea ||
+      !request.body.viewsMedian ||
+      !request.body.likesMedian ||
+      !request.body.commentsMedian
+    ) {
+      return response.status(400).send({
+        message:
+          "Required fields are missing: Idea, View Median, Like Median, Comment Median",
+      });
+    }
+    const newIdea = {
+      idea: request.body.idea,
+      viewsMedian: request.body.viewsMedian,
+      likesMedian: request.body.likesMedian,
+      commentsMedian: request.body.commentsMedian,
+    };
+
+    const idea = await Idea.create(newIdea);
+
+    return response.status(201).send(idea);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
--- backend/models/ideaModel.js
+++ backend/models/ideaModel.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const ideaSchema = mongoose.Schema(
   {
     idea: { type: String, required: true },
-    viewsAverage: { type: Number, required: true },
-    likesAverage: { type: Number, required: true },
-    commentsAverage: { type: Number, required: true },
+    viewsMedian: { type: Number, required: true },
+    likesMedian: { type: Number, required: true },
+    commentsMedian: { type: Number, required: true },
   },
   { timestamps: true }
 );
 
-export const idea = mongoose.model("idea", ideaSchema);
+export const Idea = mongoose.model("Idea", ideaSchema);
