fc5e840559a6698b543fa8bd8508cf1e25aad268
Christian Ortiz <c.ortiz313@gmail.com>
2023-09-16 14:36:19 -0400

    Functions now print videos and stats
    Rewrote the file to perform a search and then iterate over each video returned and print out its statistics

---
+++ search/searchYouTube.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const { google } = require("googleapis");
+
+const fetchVideoStatistics = (videoId) => {
+  return google
+    .youtube("v3")
+    .videos.list({
+      key: process.env.YOUTUBE_TOKEN,
+      part: "snippet,statistics",
+      id: videoId,
+    })
+    .then((response) => {
+      const { data } = response;
+      const video = data.items[0];
+      if (video) {
+        console.log(`Title: ${video.snippet.title}\n`);
+        const views = video.statistics.viewCount;
+        console.log(`Views: ${views}\n`);
+        const likes = video.statistics.likeCount;
+        console.log(`Likes: ${likes}\n`);
+        const comments = video.statistics.commentCount;
+        console.log(`Comments: ${comments}\n`);
+      } else {
+        console.log(`No data available for video with ID: ${videoId}`);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const searchAndFetchStatistics = () => {
+  return google
+    .youtube("v3")
+    .search.list({
+      key: process.env.YOUTUBE_TOKEN,
+      part: "snippet",
+      q: "Better Ideas",
+      maxResults: 3,
+    })
+    .then((response) => {
+      const { data } = response;
+      const videoItems = data.items.filter(
+        (item) => item.id.kind === "youtube#video"
+      );
+      const videoIds = videoItems.map((item) => item.id.videoId);
+      return videoIds;
+    })
+    .then((videoIds) => {
+      return Promise.all(videoIds.map(fetchVideoStatistics));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// Call the function
+searchAndFetchStatistics();
